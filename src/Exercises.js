2cfb4da1b57c54f76bab5b8929d67c4d0667e98e
evanwoo8 <evanwoo@berkeley.edu>
2024-04-27 14:07:50 -0700

    sdf

--- src/Exercises.js
+++ src/Exercises.js
@@ -1,6 +1,19 @@
 import React from "react";
 import {ChakraProvider, Box, Text, Flex, Spacer, Heading} from '@chakra-ui/react'
 import NavBar from "./NavBar";
+import Table from "Table.js"
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 
 const pageStyle = {
     color: '##4fd1af',
@@ -37,5 +50,46 @@ function Exercises() {
 </ChakraProvider>
   );
 }
+
+
+
+<TableContainer>
+  <Table variant='simple'>
+    <TableCaption>My Exercises</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>Exercise</Th>
+        <Th>Sets</Th>
+        <Th isNumeric>Weight</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>bicep curl</Td>
+        <Td>2</Td>
+        <Td isNumeric>20</Td>
+      </Tr>
+      <Tr>
+        <Td>feet</Td>
+        <Td>centimetres (cm)</Td>
+        <Td isNumeric>30.48</Td>
+      </Tr>
+      <Tr>
+        <Td>yards</Td>
+        <Td>metres (m)</Td>
+        <Td isNumeric>0.91444</Td>
+      </Tr>
+    </Tbody>
+    <Tfoot>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Tfoot>
+  </Table>
+</TableContainer>
+
+
 
 export default Exercises;
---
+++ src/Table.js
@@ -0,0 +1,64 @@
+import React from "react";
+import {ChakraProvider, Box, Text, Flex, Spacer, Heading} from '@chakra-ui/react'
+import NavBar from "./NavBar";
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
+
+function Table() {
+    return (
+  <ChakraProvider>
+    <Box sx={pageStyle}>
+    <TableContainer>
+  <Table variant='simple'>
+    <TableCaption>My Exercises</TableCaption>
+    <Thead>
+      <Tr>
+        <Th>Exercise</Th>
+        <Th>Sets</Th>
+        <Th isNumeric>Weight</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>bicep curl</Td>
+        <Td>2</Td>
+        <Td isNumeric>20</Td>
+      </Tr>
+      <Tr>
+        <Td>feet</Td>
+        <Td>centimetres (cm)</Td>
+        <Td isNumeric>30.48</Td>
+      </Tr>
+      <Tr>
+        <Td>yards</Td>
+        <Td>metres (m)</Td>
+        <Td isNumeric>0.91444</Td>
+      </Tr>
+    </Tbody>
+    <Tfoot>
+      <Tr>
+        <Th>To convert</Th>
+        <Th>into</Th>
+        <Th isNumeric>multiply by</Th>
+      </Tr>
+    </Tfoot>
+  </Table>
+</TableContainer>
+    </Box>
+  </ChakraProvider>
+    );
+  }
+
+
+
+export default Table;
